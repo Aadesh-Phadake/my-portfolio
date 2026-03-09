affe8e19b064b6db2c0a60b5d9117e8f17a4c05c
Aadesh Phadake <140011316+Aadesh-Phadake@users.noreply.github.com>
2026-03-09 16:04:30 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import './Home.css';
-import profileImg from '../assets/profile.jpg';
+import profileImg from '../assets/profile.jpeg';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
